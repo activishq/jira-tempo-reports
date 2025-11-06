b327a594a865dd4cb1025edff3c39820002bd96d
JefGreen <trempe.jeff@gmail.com>
2025-11-06 18:10:32 -0500

    add command

--- test.py
+++ test.py
@@ -5,10 +5,5 @@ sys.path.append('app')
 from reports.helpers import TokenManager
 
 
-token_manager = TokenManager()
-TokenManager()
-TokenManager()
-TokenManager()
-
-# tokens = token_manager.refresh_access_token()
-# print(tokens)
+tokens = TokenManager.refresh_access_token()
+print(tokens)
